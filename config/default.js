ab2bc0f15d3fcbe304c6bfa6299b81d5c349f628
Brian <bdombro@gmail.com>
2020-08-27 19:51:22 -0500

    Installed env config library

---
+++ config/default.js
@@ -0,0 +1,12 @@
+// In default.js
+// using defer functions is optional. See example and docs below.
+// Docs: https://github.com/lorenwest/node-config/wiki/Special-features-for-JavaScript-configuration-files
+// var defer = require('config/defer').deferConfig;
+
+module.exports = {
+  db: {
+    host: "localhost",
+    user: "postgres",
+    password: "",
+  },
+};
