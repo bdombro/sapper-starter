a98b8834c89d03a045c3cfaba62ce298a56cc2e0
Brian <bdombro@gmail.com>
2020-09-06 13:35:17 -0500

    Using piped in blog

--- src/routes/blog/index.json.ts
+++ src/routes/blog/index.json.ts
@@ -1,14 +1,21 @@
 import withCacheHandler from "../../lib/withCacheHandler"
 import posts from "./_posts"
 import type { IndexData } from "./_types"
+import { pipedAsync } from "rambdax"
 
 export const get = withCacheHandler({
   bodyBuilder: async () => {
-    const postsList: IndexData['posts'] = Array.from(posts.values()).map((post) => {
-      const {title, slug, likes} = post
-      const listPost = {title, slug, likes}
-      return listPost
-    })
+    const postsList: IndexData['posts'] = await pipedAsync(
+      posts.values(),
+      Array.from,
+      function format (posts) {
+        return posts.map((post) => {
+          const { title, slug, likes } = post
+          const listPost = { title, slug, likes }
+          return listPost
+        })
+      }
+    );
     const result: IndexData = { posts: postsList }
     return [200, JSON.stringify(result)]
   },
