1259b839f3aa21f59dd95b9cfb0bf820925e13dc
Brian <bdombro@gmail.com>
2020-11-10 13:41:12 -0600

    Add error handling to express

--- src/server.ts
+++ src/server.ts
@@ -10,25 +10,49 @@ import { jwtMiddleware } from "./lib/crypto"
 const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === "development"
 
-const defaultHeaders = function (req, res, next) {
+const defaultHeaders = function(req, res, next) {
   res.type("application/json") // default, can be overridden
   next()
 }
 
-express() // You can also use polka, but it doesn't have as many helpful features
-  .use(
-    compression(),
-    express.json(),
-    ua.express(),
-    cookies(),
-    logger("dev"),
-    sirv("static", { dev }),
-    jwtMiddleware,
-    defaultHeaders,
-    sapper.middleware({
-      session: (req) => req.auth,
-    })
-  )
-  .listen(PORT, (err) => {
-    if (err) console.log("error", err)
+const app = express() // You can also use polka, but it doesn't have as many helpful features
+
+const server =
+  app
+    .use(
+      compression(),
+      express.json(),
+      ua.express(),
+      cookies(),
+      logger("dev"),
+      sirv("static", { dev }),
+      jwtMiddleware,
+      defaultHeaders,
+      // @ts-ignore: sapper middleware is not typesafe wrt express middelware
+      sapper.middleware({
+        session: (req) => req.auth
+      })
+    )
+    .listen(PORT)
+
+server
+  .on("error", (e) => {
+    // @ts-ignore: ts doesn't know of code attribute
+    if (e.code === "EADDRINUSE")
+      console.log("Address in use :-(")
+    else
+      console.log("Unknown express error!")
+    server.close()
   })
+  .on("close", () => console.log("Http server closed."))
+
+
+function handleExit(signal) {
+  console.log(`Received exit signal ${signal}`);
+  server.close()
+  // Force exit if express doesn't close in a reasonable amount of time.
+  setTimeout(() => process.exit(1), 60000);
+}
+process.on('exit', handleExit);
+process.on('SIGINT', handleExit);
+process.on('SIGTERM', handleExit);
